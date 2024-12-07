e2f807c45de4aa0da724e466ffd620638869ce8c
Youn Sylvestre <sylvestre.youn@gmail.com>
2024-12-07 21:55:34 +0100

    logique

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,4 @@
-/*
-Objectif du jeu :
-L'ordinateur génère un nombre aléatoire entre 1 et 100.
-L'utilisateur doit deviner ce nombre.
-À chaque tentative, le programme indique si le nombre à deviner est plus grand ou plus petit.
-Le jeu se termine lorsque l'utilisateur trouve le bon nombre ou tape "quit" pour abandonner.
-
-*/
+import java.util.Scanner;
 
 public class Main {
 
@@ -17,7 +10,40 @@ public class Main {
         return (int) (Math.random() * range + min);
     }
 
+    public static void jouer() {
+        System.out.println("J'ai réfléchi et je te laisse trouver mon nombre !");
+        int nombreAleatoire = genererChiffre();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Tapez un nombre ou 'quit' pour abandonner :");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("quit")) {
+                System.out.println("Merci d'avoir joué ! Le nombre était " + nombreAleatoire);
+                break;
+            }
+
+            try {
+                int nombre = Integer.parseInt(input);
+
+                if (nombre > nombreAleatoire) {
+                    System.out.println("Moins !");
+                } else if (nombre < nombreAleatoire) {
+                    System.out.println("Plus !");
+                } else {
+                    System.out.println("GG, tu as trouvé le nombre !");
+                    break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Entrée invalide. Veuillez entrer un nombre ou 'quit'.");
+            }
+        }
+
+        scanner.close();
+    }
+
     public static void main(String[] args) {
-        System.out.println(genererChiffre());
+        jouer();
     }
 }
